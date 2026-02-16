c7aed86620aabb81972916852527c3330c5341c1
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-02-16 17:36:03 -0300

    refactor(game): reorganize imports and update router configuration

--- src/routes/game/index.ts
+++ src/routes/game/index.ts
@@ -1,9 +1,18 @@
+import { Router } from 'express';
+
 import validateBody from '@middlewares/validate-schema/index.js';
 import gameSchema from '@schemas/game/index.js';
-import { Router } from 'express';
+
+import GameRepository from '@repositories/game/index.js';
+import GameService from '@services/game/index.js';
+import GameController from '@controllers/game/index.js';
+
+const repository = GameRepository();
+const service = GameService(repository);
+const controller = GameController(service);
 
 const router = Router();
 
-router.use(validateBody(gameSchema));
+router.post('/', validateBody(gameSchema), controller.addGame);
 
 export default router;
