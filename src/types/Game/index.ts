5db4fefd563f7ce90dea24db3f2e16b8598438fd
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-02-16 13:22:02 -0300

    feature(types): add game type

---
+++ src/types/Game/index.ts
@@ -0,0 +1,25 @@
+export enum GameCurrentState { 
+    Backlog = "backlog",
+    Playing = "playing",
+    Completed = "completed"
+}
+
+export enum GamePlatforms { 
+    NES = "NES",
+    SNES = "SNES",
+    MEGA_DRIVE = "Mega Drive",
+    GAMEBOY = "Gameboy",
+    PS1 = "PS1",
+    NINTENDO_64 = "N64"
+}
+
+
+
+export interface Game { 
+    id: string,
+    title: string,
+    platform: GamePlatforms,
+    genre: string,
+    status: GameCurrentState,
+}
+
